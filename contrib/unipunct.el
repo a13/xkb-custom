b70d1bd5c952f382566b3407322c1cd20726d198
DK <beerandhot@gmail.com>
2019-09-01 12:42:59 +0300

    add package header

--- contrib/unipunct.el
+++ contrib/unipunct.el
@@ -1,3 +1,30 @@
+;;; unipunct.el --- Universal cyrillic input method   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  dk
+
+;; Author: dk <dk@somnambula>
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
 (require 'quail)
 
 ;; Modified winkeys, punctuation is the same as english
@@ -85,3 +112,5 @@ Support for Russian using UTF8 and the russian-unipunct input method.")
 (setq default-input-method "russian-unipunct")
 
 (provide 'unipunct)
+
+;;; unipunct.el ends here
